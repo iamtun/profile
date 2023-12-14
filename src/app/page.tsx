76459d402921c63566e29666ed0ee7669ecf42bf
iamtun <letuan19431791.iuh@gmail.com>
2023-12-14 22:10:04 +0700

    handle hero and change font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-import { Metadata } from 'next'
-import React from 'react'
+import { Hero } from "@/components/ui";
+import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: "Tuna.dev - Xách dép chạy deadline",
+  title: "tuna.dev - Xách dép chạy deadline",
   description: "Xin chào, đây là trang của một anh dev mới tập tành làm web",
-}
+};
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello. This is Tuna Dev</h1>
+    <main className="flex min-h-screen flex-col ">
+      <div className="container mx-auto py-4 px-12">
+        <Hero />
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
